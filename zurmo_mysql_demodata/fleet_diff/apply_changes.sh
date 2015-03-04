908d6fd22a14df2060bf63299ee57e225d17dfa0
Martin Blöchlinger <bloe@zhaw.ch>
2015-03-04 16:12:57 +0100

    * fix for demodata fleetfile

--- zurmo_mysql_demodata/fleet_diff/apply_changes.sh
+++ zurmo_mysql_demodata/fleet_diff/apply_changes.sh
@@ -2,6 +2,7 @@
 
 # replaces the image name to use the tag :demodata and rename service file
 sed -i "s/icclabcna\/zurmo_mysql/icclabcna\/zurmo_mysql:demodata/g" ../fleet/zurmo_mysql@.service
+sed -i "s/zurmo_mysql_discovery/zurmo_mysql_demodata_discovery/g" ../fleet/zurmo_mysql@.service
 mv ../fleet/zurmo_mysql@.service ../fleet/zurmo_mysql_demodata@.service
 
 # replaces the referenced image of the sidekick (add _demodata to name) and rename service file
