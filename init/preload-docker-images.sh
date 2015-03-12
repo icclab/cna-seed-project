73585eba7daf9e23eaa96f9ca39499e2827bd70c
Martin Blöchlinger <bloe@zhaw.ch>
2015-03-12 09:54:48 +0000

    + preload script can no be enabled/disabled by environment variable

--- init/preload-docker-images.sh
+++ init/preload-docker-images.sh
@@ -5,19 +5,23 @@
 # Example DOCKER_PRELOAD_IMAGES="apache:haproxy" will download icclabcna/zurmo_apache and icclabcna/zurmo_haproxy
 # If you like to change the username and prefix of the images, define the environment variables IMAGE_USERNAME, IMAGE_PREFIX
 
+ENABLED=${DOCKER_PRELOAD_ENABLED:-1}
 IMAGE_USERNAME=${DOCKER_PRELOAD_IMAGE_USERNAME:-"icclabcna"}
 IMAGE_PREFIX=${DOCKER_PRELOAD_IMAGE_PREFIX:-"zurmo_"}
 IMAGES_NAMES=${DOCKER_PRELOAD_IMAGES:-"apache:haproxy:memcache:mysql:config:application"}
 LOG_DIR=${DOCKER_PRELOAD_LOG_DIR:-"$(pwd)/logs"}
 
-mkdir -p ${LOG_DIR}
+if [ "$ENABLED" -eq 1 ]; then
 
-IFS=":"
-set ${IMAGES_NAMES}
-for item
-do
-	IMAGE_NAME=${IMAGE_USERNAME}/${IMAGE_PREFIX}${item}
-	echo "Pulling ${IMAGE_NAME}"
-	docker pull -a ${IMAGE_NAME} > ${LOG_DIR}/info_${item}.log &
-done
-wait
+	mkdir -p ${LOG_DIR}
+
+	IFS=":"
+	set ${IMAGES_NAMES}
+	for item
+	do
+		IMAGE_NAME=${IMAGE_USERNAME}/${IMAGE_PREFIX}${item}
+		echo "Pulling ${IMAGE_NAME}"
+		docker pull -a ${IMAGE_NAME} > ${LOG_DIR}/info_${item}.log &
+	done
+	wait
+fi
