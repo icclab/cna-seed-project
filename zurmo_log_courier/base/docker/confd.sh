61d6951510678c1f912f26223e2021461a752f9c
Martin Blöchlinger <bloe@zhaw.ch>
2015-05-08 09:29:42 +0000

    + log-courier for apache ships also collectd logs (cpu,load,memory)

--- zurmo_log_courier/base/docker/confd.sh
+++ zurmo_log_courier/base/docker/confd.sh
@@ -11,6 +11,8 @@ echo "[${CONTAINER_NAME}] booting container"
 
 mkdir -p /var/log/log-courier && touch /var/log/log-courier/log-courier.log
 
+sed -i s/%hostname%/${LOG_SRC_HOSTNAME}/g /etc/confd/templates/*.tmpl
+
 # Try to make initial configuration every 5 seconds until successful
 until confd -onetime -node $ETCD_ENDPOINT -config-file ${TOML_PATH}; do
     echo "[${CONTAINER_NAME}] waiting for confd to create initial ${CONTAINER_NAME} configuration."
