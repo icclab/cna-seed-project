e638c8299f1ed530e9180b7160b2937637bba26a
Martin Blöchlinger <bloe@zhaw.ch>
2015-05-22 09:18:13 +0000

    * install logstash courier plugin the right way (not git clone but download the release source code)

--- zurmo_logstash/docker/install_courier_plugin.sh
+++ zurmo_logstash/docker/install_courier_plugin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/driskell/log-courier
-cd log-courier
+wget https://github.com/driskell/log-courier/archive/v1.6.tar.gz
+tar -zxvf v1.6.tar.gz
+cd log-courier-1.6/
 make gem
-cd /logstash-1.4.2.tar.gz/logstash-1.4.2
+cd /logstash-1.4.2.tar.gz/logstash-1.4.2 
 export GEM_HOME=vendor/bundle/jruby/1.9
-java -jar vendor/jar/jruby-complete-1.7.11.jar -S gem install /log-courier/log-courier-1.6*.gem
-cd /log-courier
+java -jar vendor/jar/jruby-complete-1.7.11.jar -S gem install /log-courier-1.6/log-courier-1.6*.gem
+cd /log-courier-1.6
 cp -rvf lib/logstash /logstash-1.4.2.tar.gz/logstash-1.4.2/lib/
