075be65719ab9e9176c2ce6d9cc6b6251f253d52
Giovanni Toffetti <toff@zhaw.ch>
2015-11-05 19:58:30 +0100

    preparing to merge integration branch into master

--- helper-scripts/clean_up_coreos_to_make_ami.sh
+++ helper-scripts/clean_up_coreos_to_make_ami.sh
@@ -1,8 +1,8 @@
 cd /tmp
-wget https://raw.githubusercontent.com/icclab/cna-seed-project/integration/init/fleet-files
+wget https://raw.githubusercontent.com/icclab/cna-seed-project/master/init/fleet-files
 wget -i fleet-files
 for unit in $(find . -name "*.service" ); do grep -o -e "icclabcna/.*:" $unit; done | sort | uniq > units.txt
-cat units.txt | xargs -I \{\} docker pull \{\}latest
+cat units.txt | xargs -I \{\} docker pull \{\}master
 sudo systemctl stop etcd2
 sudo rm -rf /var/lib/etcd2/*
 sudo rm -f /etc/systemd/system/etcd*
