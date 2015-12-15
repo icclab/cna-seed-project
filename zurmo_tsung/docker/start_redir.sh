50b3a698da2b03c7274c965365dd022b588d7685
oezs <o.oezsu@gmail.com>
2015-12-15 14:24:50 +0100

    Sed command

--- zurmo_tsung/docker/start_redir.sh
+++ zurmo_tsung/docker/start_redir.sh
@@ -3,5 +3,7 @@
 #redir --lport=80 --laddr=127.0.0.1 --caddr=web --cport=80
 #sed -Ei 's/LINK/$LINK/g' tsung_sample_config.xml
 
+echo $LINK
+echo "Link: ${LINK}"
 sed -i "s@LINK@${LINK}@g" tsung_sample_config.xml
 #tsung -f tsung_sample_config.xml start
