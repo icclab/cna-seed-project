1a8edcef5161a9637996ebe89982e6ef5f7c7ac0
oezs <o.oezsu@gmail.com>
2015-12-15 13:35:13 +0100

    Fixing sed command

--- zurmo_tsung/docker/start_redir.sh
+++ zurmo_tsung/docker/start_redir.sh
@@ -3,5 +3,5 @@
 #redir --lport=80 --laddr=127.0.0.1 --caddr=web --cport=80
 #sed -Ei 's/LINK/$LINK/g' tsung_sample_config.xml
 
-sed -i -e "s@LINK@$LINK@g" tsung_sample_config.xml
+sed -e "s@LINK@${LINK}@ig" tsung_sample_config.xml
 #tsung -f tsung_sample_config.xml start
