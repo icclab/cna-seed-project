fe1df0a194deac11c2dd9cd0acffd3f6655e7c17
oezs <o.oezsu@gmail.com>
2015-12-15 09:20:58 +0100

    Updated zurmo_tsung

--- zurmo_tsung/docker/start_redir.sh
+++ zurmo_tsung/docker/start_redir.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-redir --lport=80 --laddr=127.0.0.1 --caddr=web --cport=80
+#redir --lport=80 --laddr=127.0.0.1 --caddr=web --cport=80
+#sed -Ei 's/LINK/$LINK/g' tsung_sample_config.xml
+
+sed -i "s/LINK/$LINK/g" tsung_sample_config.xml
+tsung -f tsung_sample_config.xml start
