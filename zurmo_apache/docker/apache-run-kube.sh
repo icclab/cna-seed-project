4a4c8fb188ef651772947c2fe6c618c83a631686
oezs <o.oezsu@gmail.com>
2015-12-18 14:06:14 +0100

    Updated entrypoint

--- zurmo_apache/docker/apache-run-kube.sh
+++ zurmo_apache/docker/apache-run-kube.sh
@@ -7,7 +7,7 @@ collectd -C /collectd.conf
 
 # set file permissions of www-directory
 #/set_data_permissions.sh
-chown -R www-data /zurmo
+#chown -R www-data /zurmo
 
 sed -i "s@MYSQLHOST@${MYSQLHOST}@g" config/perInstance.php
 
